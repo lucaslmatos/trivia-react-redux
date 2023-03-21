8c32a4bd6ca8d545b2e64c1d713dda0c042ffb74
João Ricardo Viana <joaorviana98@gmail.com>
2023-03-21 15:44:02 -0300

    Req18 button Home

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          data-testid="btn-go-home"
+          onClick={ () => {
+            history.push('/');
+          } }
+        >
+          Home
+
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {}.isRequired;
 
 export default Ranking;
