1bfcdcfdec83bcc5b49acd8f0e69a67b9ca4d961
MauricioFernanddes <mauriciojrfs@hotmail.com>
2023-03-23 17:53:19 -0300

    requesito 17

---
+++ src/tests/feedback.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Feedback from "../pages/Feedback";
+import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
+import { act, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+
+describe('Testando a página Feedback', () => {
+  it('testando se ao clicar no botao "play again", o jogador é redirecionado a pagina inicial', () => {
+    const {history } = renderWithRouterAndRedux(<App/>)
+    act(() => {
+      history.push('/feedback')
+    })
+    const playAgainBtn = screen.getByRole('button', {
+      name: /play again/i
+    })
+    userEvent.click(playAgainBtn)
+    const pathname = history.location.pathname
+    expect(pathname).toBe('/')
+  });
+
+  it('testando se ao clicar no botao "Ranking", o jogador é redirecionado a tela de ranking', () => {
+    const {history } = renderWithRouterAndRedux(<App/>)
+    act(() => {
+      history.push('/feedback')
+    })
+    const rankingBtn = screen.getByRole('button', {
+      name: /ranking/i
+    })
+    userEvent.click(rankingBtn)
+    const pathname = history.location.pathname
+    expect(pathname).toBe('/ranking')
+  })
+});
